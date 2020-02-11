ce43642d267c0d3a0f97271fd48a452dabc8d5f7
Trevor West <t.west55@hotmail.com>
2020-02-10 20:58:39 -0600

    Test in VS 2019

--- PerformanceSystem/PerformanceSystem/Program.cs
+++ PerformanceSystem/PerformanceSystem/Program.cs
@@ -7,6 +7,7 @@ namespace PerformanceSystem
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			Console.WriteLine("hello again");
 			//will prevent window form closing until a key is pressed
 			Console.Read();
 		}
