7fc9db879b32163e2ddd0ebda8bc9a9fdea49326
twest <trevor.west@mnsu.edu>
2020-02-18 16:57:16 -0600

    Remade the project

---
+++ PerformanceAPI/PerformanceAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PerformanceAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
