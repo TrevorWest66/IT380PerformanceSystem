6310c68d2e3024fddf3bf827eeaf69298b8c94e6
Ellie M <elliejeanmcd@gmail.com>
2020-03-04 22:16:46 -0600

    Added employee class and model for the employees page

---
+++ PerformanceAPI/PerformanceAPI/Models/EmployeeModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceAPI.Models
+{
+    public class EmployeeModel
+    {
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeMiddleIntial { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string EmployeeGender { get; set; }
+        public string EmployeeID { get; set; }
+        public string EmployeeDateOfLastReview { get; set; }
+        public string EmployeeActualPreformanceReview { get; set; }
+        public string EmployeeCurrentSalary { get; set; }
+        public string EmployeePosition { get; set; }
+        public string EmployeeProjectedPreformanceReview { get; set; }
+        public string EmployeeProjectedSalaryIncrease { get; set; }
+
+        /**
+         * Gets the full name of the employee formated as LastName, FirstName MiddleIntial
+         */
+        public string getFormatedEmployeeFullName() 
+        {
+            string fullName = String.Format("{0}, {1} {2}", EmployeeLastName, EmployeeFirstName, EmployeeMiddleIntial);
+            return fullName;
+        }
+
+        /**
+         * Returns the date 1 year from the last performance review
+         */
+        public string getDateOfNextReview()
+        {
+            int yearOfLastReview = int.Parse(EmployeeDateOfLastReview.Substring(7));
+            int yearOfNextReview = yearOfLastReview + 1;
+            string dateOfNextReview = (EmployeeDateOfLastReview.Substring(0, 7)) + (yearOfNextReview.ToString());
+            return dateOfNextReview;
+        }
+
+    }
+}
