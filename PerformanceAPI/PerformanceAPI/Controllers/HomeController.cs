fd1f8f1b6738cdad7da726816bdfeffc01791099
Trevor West <t.west55@hotmail.com>
2020-04-09 15:21:43 -0500

    Added chart.js

--- PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
+++ PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace PerformanceAPI.Controllers
             return View();
         }
 
+        public IActionResult PredictionSummary()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- PerformanceAPI/PerformanceAPI/wwwroot/js/site.js
+++ PerformanceAPI/PerformanceAPI/wwwroot/js/site.js
@@ -12,4 +12,3 @@ for (i = 0; i < toggler.length; i++) {
         this.classList.toggle("caret-down");
     });
 }
-
