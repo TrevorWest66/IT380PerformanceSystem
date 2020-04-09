1e44966e7df43ae22583c02a5bf7286b97357127
Trevor West <t.west55@hotmail.com>
2020-04-09 16:15:47 -0500

    Added the reports pages

--- PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
+++ PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
@@ -48,6 +48,11 @@ namespace PerformanceAPI.Controllers
             return View();
         }
 
+        public IActionResult ActualSummary()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ PerformanceAPI/PerformanceAPI/wwwroot/js/PredictionsReport.js
@@ -0,0 +1,28 @@
+﻿var ctx1 = document.getElementById('SummaryBudgetChart').getContext('2d');
+var predictionChart = new Chart(ctx1, {
+    type: 'pie',
+    data: {
+        labels: ['Unused', 'Used'],
+        datasets: [{
+            label: 'Budget Used',
+            data: [20, 80],
+            backgroundColor: [
+                '#8E8C8C',
+                '#FF2015'
+            ],
+            borderColor: [
+                '#FFFFFF',
+                '#FFFFFF'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Budget Used'
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+    }
+});
