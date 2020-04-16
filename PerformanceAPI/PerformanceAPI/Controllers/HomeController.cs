37f2153bb4c5987b493ae3345e9cb5f5e26a4400
Ellie M <elliejeanmcd@gmail.com>
2020-04-16 12:06:28 -0500

    Added projections page

--- PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
+++ PerformanceAPI/PerformanceAPI/Controllers/HomeController.cs
@@ -55,6 +55,11 @@ namespace PerformanceAPI.Controllers
             return View();
         }
 
+        public IActionResult Projections()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
