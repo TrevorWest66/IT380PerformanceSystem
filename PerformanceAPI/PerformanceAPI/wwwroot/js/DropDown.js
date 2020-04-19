e8d22fc9ad447720724189ae7653c179d1955a2a
Trevor West <t.west55@hotmail.com>
2020-04-19 16:23:37 -0500

    add javascript for drop down

---
+++ PerformanceAPI/PerformanceAPI/wwwroot/js/DropDown.js
@@ -0,0 +1,7 @@
+﻿console.log("this is working");
+
+document.getElementById("expandable").addEventListener("click", openMenu);
+
+function openMenu() {
+	document.getElementById("drop-down").classList.toggle("active");
+}
